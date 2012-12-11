80b4bfa4be6a3c92be16de35d59c91ced5904181
Xaris Tsimpouris <xaris.tsimpouris@gmail.com>
2012-12-11 23:13:13 +0200

    Major changes..

--- normalize-for-tablet.bat
+++ normalize-for-tablet.bat
@@ -1,42 +1,73 @@
+cls
 @echo off
-echo Script by http://1024.gr
+call :ColorText 0E "Script by 1024.gr"
+
+setlocal enableextensions enabledelayedexpansion
 
 pushd %~dp0
 
-del %1\log.txt > nul
-echo %time% Starting batch process > %1\log.txt
-
-for %%x in (%1\*.avi) do (
-  echo Proccessing file %%x...
-  echo %time% Proccessing file %%x... >> %1\log.txt
-  
-  echo %time% Extracting/Converting video..
-  echo %time% Extracting/Converting video.. >> %1\log.txt
-  ffmpeg -loglevel panic -i "%%x" -map 0:0 -vcodec libx264 -preset fast -profile:v baseline -crf 28 -threads 0 -y "%%x_clean.avi" >> %1\log.txt
-
-  echo %time% Extracting/Decoding audio..
-  echo %time% Extracting/Decoding audio.. >> %1\log.txt
-  ffmpeg.exe -loglevel panic -i "%%x" -ac 2 -y "%%x_clean.wav" >> %1\log.txt
-   
-  echo %time% Getting max amplitude..
-  echo %time% Getting max amplitude.. >> %1\log.txt
-  sox "%%x_clean.wav" -n stat -v 2> "%%x_apl.txt"
-  Set /P apl_value= < "%%x_apl.txt"
-   
-  echo %time% Normalizing decoded audio.. gain: %apl_value%
-  echo %time% Normalizing decoded audio.. gain: %apl_value% >> %1\log.txt
-  sox --show-progress -v %apl_value% "%%x_clean.wav" "%%x_norm.wav" >> %1\log.txt
-
-  echo %time% Creating new video file, encoding audio..
-  echo %time% Creating new video file, encoding audio.. >> %1\log.txt
-  ffmpeg.exe -loglevel panic -i "%%x_clean.avi" -i "%%x_norm.wav" -vcodec copy -acodec libvo_aacenc -ab 128k -y "%%x" >> %1\log.txt
-
-  :clear
-  echo %time% Cleaning..
-  del "%%x_clean.wav"
-  del "%%x_clean.avi"
-  del "%%x_norm.wav"
-  del "%%x_apl.txt"
+call :ColorText 0A "Starting batch process"
+for %%x in ("%~1\*.avi" "%~1\*.mp4") do (
+  CALL :process "%%x"
 )
 
 popd
+
+:process
+  echo Proccessing file %~1...
+  
+  call :ColorText 0A "Extracting & Converting video.."
+  ffmpeg -i "%~1" -map 0:0 -vcodec libx264 -preset fast -profile:v baseline -crf 28 -threads 0 -y "%~1_clean.avi"
+
+  call :ColorText 0A "Extracting & Decoding audio.."
+  ffmpeg.exe -i "%~1" -ac 2 -y "%~1_clean.wav"
+   
+  call :ColorText 0A "Getting max amplitude.."
+  sox "%~1_clean.wav" -n stat -v 2> "%~1_apl.txt"
+  
+  Set /P APL_VALUE= < "%~1_apl.txt"
+  
+  call :ColorText 0A "Normalizing decoded audio with gain %APL_VALUE%.."
+  sox --show-progress -v %APL_VALUE% "%~1_clean.wav" "%~1_norm.wav"
+
+  call :ColorText 0A "Creating new video file, encoding audio.."
+  ffmpeg.exe -i "%~1_clean.avi" -i "%~1_norm.wav" -vcodec copy -acodec libvo_aacenc -ab 128k -y "%~1"
+
+  call :ColorText 0A "Cleaning.."
+  del "%~1_clean.wav"
+  del "%~1_clean.avi"
+  del "%~1_norm.wav"
+  del "%~1_apl.txt"
+
+goto :eof
+
+:: http://stackoverflow.com/questions/2586012/how-to-change-the-text-color-of-comments-line-in-a-batch-file
+
+:ColorText Color String
+::
+:: Prints String in color specified by Color.
+::
+::   Color should be 2 hex digits
+::     The 1st digit specifies the background
+::     The 2nd digit specifies the foreground
+::     See COLOR /? for more help
+::
+::   String is the text to print. All quotes will be stripped.
+::     The string cannot contain any of the following: * ? < > | : \ /
+::     Also, any trailing . or <space> will be stripped.
+::
+::   The string is printed to the screen without issuing a <newline>,
+::   so multiple colors can appear on one line. To terminate the line
+::   without printing anything, use the ECHO( command.
+::
+setlocal
+set /p "=%time% " <nul
+pushd %temp%
+for /F "tokens=1 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  <nul set/p"=%%a" >"%~2"
+)
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
+echo(
+popd
+exit /b
