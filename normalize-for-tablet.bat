bd7c5e1ef86731f0b68ba2288332e7b964d06e14
Xaris Tsimpouris <xaris.tsimpouris@gmail.com>
2012-12-11 13:46:07 +0200

    Logging capability, less screen verbose

--- normalize-for-tablet.bat
+++ normalize-for-tablet.bat
@@ -3,32 +3,40 @@ echo Script by http://1024.gr
 
 pushd %~dp0
 
+del %1\log.txt > nul
+echo %time% Starting batch process > %1\log.txt
+
 for %%x in (%1\*.avi) do (
   echo Proccessing file %%x...
+  echo %time% Proccessing file %%x... >> %1\log.txt
   
   echo %time% Extracting/Converting video..
-  ffmpeg -loglevel panic -i "%%x" -map 0:0 -vcodec libx264 -preset fast -profile:v baseline -crf 28 -threads 0 -y "%%x_clean.avi" > nul
+  echo %time% Extracting/Converting video.. >> %1\log.txt
+  ffmpeg -loglevel panic -i "%%x" -map 0:0 -vcodec libx264 -preset fast -profile:v baseline -crf 28 -threads 0 -y "%%x_clean.avi" >> %1\log.txt
 
   echo %time% Extracting/Decoding audio..
-  ffmpeg.exe -loglevel panic -i "%%x" -ac 2 -y "%%x_clean.wav" > nul
+  echo %time% Extracting/Decoding audio.. >> %1\log.txt
+  ffmpeg.exe -loglevel panic -i "%%x" -ac 2 -y "%%x_clean.wav" >> %1\log.txt
    
   echo %time% Getting max amplitude..
-  sox "%%x_clean.wav" -n stat -v 2> "%%x_apl.txt" > nul
+  echo %time% Getting max amplitude.. >> %1\log.txt
+  sox "%%x_clean.wav" -n stat -v 2> "%%x_apl.txt"
   Set /P apl_value= < "%%x_apl.txt"
    
   echo %time% Normalizing decoded audio.. gain: %apl_value%
-  sox --show-progress -v %apl_value% "%%x_clean.wav" "%%x_norm.wav" > nul
+  echo %time% Normalizing decoded audio.. gain: %apl_value% >> %1\log.txt
+  sox --show-progress -v %apl_value% "%%x_clean.wav" "%%x_norm.wav" >> %1\log.txt
 
-  rundll32 user32.dll,MessageBeep -1
   echo %time% Creating new video file, encoding audio..
-  ffmpeg.exe -loglevel panic -i "%%x_clean.avi" -i "%%x_norm.wav" -vcodec copy -acodec libvo_aacenc -ab 128k -y "%%x" > nul
+  echo %time% Creating new video file, encoding audio.. >> %1\log.txt
+  ffmpeg.exe -loglevel panic -i "%%x_clean.avi" -i "%%x_norm.wav" -vcodec copy -acodec libvo_aacenc -ab 128k -y "%%x" >> %1\log.txt
 
   :clear
   echo %time% Cleaning..
-  del %1_clean.wav
-  del %1_clean.avi
-  del %1_norm.wav
-  del %1_apl.txt
+  del "%%x_clean.wav"
+  del "%%x_clean.avi"
+  del "%%x_norm.wav"
+  del "%%x_apl.txt"
 )
 
 popd
